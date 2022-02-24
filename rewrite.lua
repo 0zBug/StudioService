376769e9b4af2e8d143e76455e0b926aad8427c4
Bug <77031032+0zBug@users.noreply.github.com>
2022-02-24 13:34:25 -0600

    Update rewrite.lua

--- rewrite.lua
+++ rewrite.lua
@@ -1,7 +1,7 @@
-
 local Build = {}
 
 local xml
+local indent = 0
 local Datatypes = {
     Source = {"ProtectedString", function(v)
         return "<![CDATA[" .. v .. "]]>"
@@ -24,19 +24,24 @@ local Datatypes = {
 }
 
 local function BuildInstance(Instance)
-    xml = xml .. '<Item class="' .. Instance.ClassName .. '" referent="RBX620AF4060A8D4746BB58908C5E089749">\n<Properties>\n'
+    xml = xml .. string.rep('\t', indent) .. '<Item class="' .. Instance.ClassName .. '" referent="RBX620AF4060A8D4746BB58908C5E089749">\n'
+    indent = indent + 1
+    xml = xml .. string.rep('\t', indent) .. '<Properties>\n'
 
+    indent = indent + 1
     if Instance.Properties then
         for i,v in next, Instance.Properties do
             if Datatypes[i] then
-                xml = xml .. '<' .. Datatypes[i][1] .. ' name="' .. i .. '">' .. Datatypes[i][2](v) .. '</' .. Datatypes[i][1] .. '>\n' 
+                xml = xml .. string.rep('\t', indent) ..  '<' .. Datatypes[i][1] .. ' name="' .. i .. '">' .. Datatypes[i][2](v) .. '</' .. Datatypes[i][1] .. '>\n' 
             else
-                xml = xml .. '<' .. type(v) .. ' name="' .. i .. '">' .. tostring(v) .. '</' .. type(v) .. '>\n' 
+                xml = xml .. string.rep('\t', indent) .. '<' .. type(v) .. ' name="' .. i .. '">' .. tostring(v) .. '</' .. type(v) .. '>\n' 
             end
         end
     end
+    indent = indent - 1
     
-    xml = xml .. '</Properties>\n'
+    xml = xml .. string.rep('\t', indent) .. '</Properties>\n'
+    indent = indent - 1
     
     if Instance.Children then
         for i,v in next, Instance.Children do
@@ -44,26 +49,32 @@ local function BuildInstance(Instance)
         end
     end
     
-    xml = xml .. '</Item>\n'
+    xml = xml .. string.rep('\t', indent) .. '</Item>\n'
 end
 
 local StudioService = {}
 
 function StudioService:Build(Name)
-    xml = '<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4">'
+    xml = '<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4">\n'
     
+    indent = indent + 1
     for Parent, Children in next, Build do
-        xml = xml .. '<Item class="' .. Parent .. '" referent="RBX6BA637DD8B9C44D385DF70A48988C0D5">\n'
+        xml = xml .. string.rep('\t', indent) .. '<Item class="' .. Parent .. '" referent="RBX6BA637DD8B9C44D385DF70A48988C0D5">\n'
        
+        indent = indent + 1
         for i,v in next, Children do
             BuildInstance(v)
         end
+        indent = indent - 1
         
-        xml = xml .. '</Item>\n'
+        xml = xml .. string.rep('\t', indent) .. '</Item>\n'
     end
+    indent = indent - 1
     
     xml = xml .. '</roblox>'
+    
     writefile(Name .. ".rbxlx", xml)
+    return xml
 end
 
 function StudioService.new(ClassName, Parent)
